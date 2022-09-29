01fc27079be19ed93890bf8175ac17425cd01780
김신철 <zzangsin2002@gmail.com>
2022-09-29 16:07:12 +0900

    tmp

--- gorda/src/pages/DonationList.jsx
+++ gorda/src/pages/DonationList.jsx
@@ -21,9 +21,10 @@ function DonationList() {
   useEffect(() => {
     async function testlist() {
       if (campaigns[0] != undefined) {
-        console.log("111111111111111111111", campaigns[0]);
-        const tmp = await Campaign(campaigns[0]).methods.getSummary().call();
-        setInfos(tmp);
+        for (let i = 0; i < campaigns.length; i++) {
+          const tmp = await Campaign(campaigns[i]).methods.getSummary().call();
+          setInfos((infos) => [...infos, tmp]);
+        }
       }
     }
 
@@ -60,18 +61,20 @@ function DonationList() {
         </div>
         <div className="test">
           <div className="page_card">
-            <Link to="/detail">
-              <DonationListCard
-                imgURL={infos[10]}
-                title={infos[5]}
-                description={infos[9]}
-                target={infos[11]}
-              />
-            </Link>
-            <DonationListCard />
-            <DonationListCard />
-            <DonationListCard />
-            <DonationListCard />
+            {infos.map((item, key) => {
+              return (
+                <Link to={`/${campaigns[key]}`}>
+                  <DonationListCard
+                    category={item[7]}
+                    imgURL={item[10]}
+                    title={item[5]}
+                    description={item[9]}
+                    target={item[11]}
+                  />
+                </Link>
+              );
+            })}
+
             <DonationListCard />
             <DonationListCard />
           </div>
